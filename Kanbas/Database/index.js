f17ccae0b98ad1d9b03d885f495a68b16b023c2d
Eli Kinkade <ekinkade21@gmail.com>
2024-04-03 20:05:19 -0400

    first node commit

---
+++ Kanbas/Database/index.js
@@ -0,0 +1,7 @@
+import courses from "./courses.js";
+import assignments from "./assignments.js";
+import users from "./users.js";
+import grades from "./grades.js";
+import modules from "./modules.js";
+import enrollment from "./enrollment.js";
+export default {courses, assignments, users, grades, modules, enrollment};
