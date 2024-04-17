dadbcfa7dcc200affe3cab6ac342add42b12f645
Eli Kinkade <ekinkade21@gmail.com>
2024-04-17 14:23:57 -0400

    retrieve records

--- App.js
+++ App.js
@@ -1,14 +1,10 @@
-import express from 'express';
-import Hello from './Hello.js';
-import Lab5 from './Lab5.js';
-import CourseRoutes from "./Kanbas/courses/routes.js";
-import cors from 'cors';
-import ModuleRoutes from './Kanbas/modules/routes.js';
-const app = express()
-app.use(cors())
-app.use(express.json())
-CourseRoutes(app);
-ModuleRoutes(app);
-Hello(app)
-Lab5(app)
-app.listen(process.env.PORT || 4000)
+import express from "express";
+import mongoose from "mongoose";
+import UserRoutes from "./Users/routes.js";
+import cors from "cors";
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
+const app = express();
+app.use(cors());
+app.use(express.json());
+UserRoutes(app);
+app.listen(4000);
--- Kanbas/Database/index.js
+++ Kanbas/Database/index.js
@@ -1,7 +1,7 @@
-import courses from "./courses.json";
-import assignments from "./assignments.json";
-import users from "./users.json";
+import courses from "./courses.json" assert { type: 'json' };
+import assignments from "./assignments.json" assert { type: 'json' };
+import users from "./users.json" assert { type: 'json' };
 import grades from "./grades.js";
-import modules from "./modules.json";
+import modules from "./modules.json" assert { type: 'json' };
 import enrollment from "./enrollment.js";
 export default {courses, assignments, users, grades, modules, enrollment};
